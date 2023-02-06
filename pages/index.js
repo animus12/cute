98eb8dde24b120609c9a8188ea94e16e45b6f6fa
animus12 <mypersonalacount4@gmail.com>
2023-02-06 14:21:10 +0800

    testing commit

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
 			<div className="container mx-auto px-4">
 				<h1 className="text-3xl font-bold underline text-red-600">
 					Hello world!
+					Hello from edit
 				</h1>
 			</div>
 		</div>
